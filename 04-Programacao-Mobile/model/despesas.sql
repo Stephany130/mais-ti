421d9a300cf44c7a89d901b7334776b15098a37c
Stephany_Catharina13 <72426145+Stephany130@users.noreply.github.com>
2026-03-30 19:52:33 -0300

    exercicio para front de condominio

---
+++ 04-Programacao-Mobile/model/despesas.sql
@@ -0,0 +1,16 @@
+INSERT INTO despesas (id_predio,id_contato,nome,valor_total,vencimento,foi_pago,id_documento,categoria,grupo_referencia) VALUES
+
+(1,NULL,'Conta de Água',168.00,'2026-01-10','sim',1,'consumo','2026-01'),
+(1,NULL,'Conta de Energia',190.00,'2026-01-10','sim',2,'consumo','2026-01'),
+(1,NULL,'Salário Zelador',1600.00,'2026-01-05','sim',3,'salario','2026-01'),
+(1,NULL,'Encargos Trabalhistas Zelador',800.00,'2026-01-05','sim',4,'encargos','2026-01'),
+
+(1,NULL,'Conta de Água',155.00,'2026-02-10','sim',5,'consumo','2026-02'),
+(1,NULL,'Conta de Energia',210.00,'2026-02-10','sim',6,'consumo','2026-02'),
+(1,NULL,'Salário Zelador',1600.00,'2026-02-05','sim',7,'salario','2026-02'),
+(1,NULL,'Encargos Trabalhistas Zelador',800.00,'2026-02-05','sim',8,'encargos','2026-02'),
+
+(1,NULL,'Conta de Água',162.00,'2026-03-10','sim',9,'consumo','2026-03'),
+(1,NULL,'Conta de Energia',195.00,'2026-03-10','sim',10,'consumo','2026-03'),
+(1,NULL,'Salário Zelador',1600.00,'2026-03-05','sim',11,'salario','2026-03'),
+(1,NULL,'Encargos Trabalhistas Zelador',800.00,'2026-03-05','sim',12,'encargos','2026-03');
